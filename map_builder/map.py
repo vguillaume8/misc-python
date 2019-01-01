340224f81aedc1a3a8925b787182e438d49bff08
Vinston Guillaume <vinstonguillaume@gmail.com>
2019-01-01 03:34:00 -0500

    map builder

--- map_builder/map.py
+++ map_builder/map.py
@@ -1,11 +1,32 @@
-# Import Library
+#Import Library
 import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
 
-# Create base map
-map = folium.Map(location=[37.296933, -121.9574983], zoom_start = 8)
+#Load Data
+data = pd.read_csv("Volcanoes_USA.txt")
+lat = data['LAT']
+lon = data['LON']
+elevation = data['ELEV']
 
-# Add Marker
-folium.Marker(location=[37.4074687,-122.086669])
+#Function to change colors
+def color_change(elev):
+    if(elev < 1000):
+        return('green')
+    elif(1000 <= elev <3000):
+        return('orange')
+    else:
+        return('red')
 
-# Save the map
+#Create base map
+map = folium.Map(location=[37.296933,-121.9574983], zoom_start = 5, tiles = "CartoDB dark_matter")
+
+#Create Cluster
+marker_cluster = MarkerCluster().add_to(map)
+
+#Plot Markers and add to 'marker_cluster'
+for lat, lon, elevation in zip(lat, lon, elevation):
+    folium.CircleMarker(location=[lat, lon], radius = 9, popup=str(elevation)+" m", fill_color=color_change(elevation), color="gray", fill_opacity = 0.9).add_to(marker_cluster)
+
+#Save the map
 map.save("map.html")
