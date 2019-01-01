14eefa96d5bae57851de46515e477fd834d05c63
Vinston Guillaume <vinstonguillaume@gmail.com>
2019-01-01 03:21:20 -0500

    supports multiple definitions

--- dictionary_defintion_lookup/dictionary.py
+++ dictionary_defintion_lookup/dictionary.py
@@ -1,10 +1,12 @@
 # import library
 import json
+import difflib #Text proccessing servuces
+from difflib import get_close_matches
 
 # Loading the json data as python dictionary
 data = json.load(open("dictionary.json"))
 
-# Function for retriving definition
+# Function for retrieving definition
 def retrive_definiton(word):
     # Removing case-sensitivity from the program
     # For example 'Rain' and 'rain' will give the same output
@@ -18,11 +20,37 @@ def retrive_definiton(word):
     # To make sure the program return the definition of words that start with a capital leter
     elif word.title() in data:
         return data[word.title()]
-    else:
-        return ("The word does not exist, please double check it.")
+    # To make sure the program returns the definiton of acronyms
+    elif word.upper() in data:
+        return data[word.upper()]
+
+    # searches for a similar word
+    elif len(get_close_matches(word, data.keys())) > 0:
+        action =  raw_input("Did you mean %s instead? [y or n]: " % get_close_matches(word, data.keys())[0])
+
+        # If the answer is yes, retrieve definition of suggested word
+        if(action == "y"):
+            return data[get_close_matches(word, data.keys())[0]]
+
+        elif (action == "n"):
+            return ("The word doesn't exist, yet.")
+
+        else:
+            return ("We don't understand you entry")
+
 
 # Input from user
-word_user = input("Enter a word: ")
+word_user = raw_input("Enter a word: ")
 
 # Retrive the definition using function and print the result
-print(retrive_definiton(word_user))
+output = (retrive_definiton(word_user))
+
+# If a word has more than one definition, print them recursively
+if type(output) == list:
+    for item in output:
+        print "-", item
+
+# For words having single definition
+else:
+    print "-", output
+
