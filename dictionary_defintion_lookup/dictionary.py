4a16d4257688c23382cdae5f975a62ae84403ce5
Vinston Guillaume <vinstonguillaume@gmail.com>
2019-01-01 03:07:26 -0500

    converts to lowercase

--- dictionary_defintion_lookup/dictionary.py
+++ dictionary_defintion_lookup/dictionary.py
@@ -6,7 +6,20 @@ data = json.load(open("dictionary.json"))
 
 # Function for retriving definition
 def retrive_definiton(word):
-    return data[word]
+    # Removing case-sensitivity from the program
+    # For example 'Rain' and 'rain' will give the same output
+    # Converting all letters to lower because out data is in that format
+    word = word.lower()
+
+
+    # Check for non existing words
+    if word in data:
+        return data[word]
+    # To make sure the program return the definition of words that start with a capital leter
+    elif word.title() in data:
+        return data[word.title()]
+    else:
+        return ("The word does not exist, please double check it.")
 
 # Input from user
 word_user = input("Enter a word: ")
